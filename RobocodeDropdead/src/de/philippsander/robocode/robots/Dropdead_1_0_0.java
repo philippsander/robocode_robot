565e74abfdb184c42d99541f92fb53f640faf834
Philipp Sander <philippsander88@gmx.net>
2013-08-31 10:01:47 +0200

    created the empty robot

---
+++ RobocodeDropdead/src/de/philippsander/robocode/robots/Dropdead_1_0_0.java
@@ -0,0 +1,12 @@
+package de.philippsander.robocode.robots;
+
+import robocode.AdvancedRobot;
+
+/**
+ * 
+ * @author Philipp Sander
+ *
+ */
+public class Dropdead_1_0_0 extends AdvancedRobot {
+
+}
